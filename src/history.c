2cd3d4195145ded5684d0278d00d0ac3a0867319
macontreras11 <macontreras11@miners.utep.edu>
2024-11-13 08:56:33 -0700

    added comments

--- src/history.c
+++ src/history.c
@@ -5,78 +5,84 @@
 #include "history.h"
 
 List *init_history(){
-  List *lst= (List*) malloc(sizeof(List));
+  List *lst= (List*) malloc(sizeof(List));//allocate memory
   lst ->root =NULL;
-  return lst;
+  return lst;//return pointer to created list
 }
 
 
 void add_history(List *list, char *str){
-  if(list==NULL || str==NULL){
-    return; }
+  if(list==NULL || str==NULL){//check if list or pintter in null
+    return;
+  }
 
-  Item *newItem =(Item*)malloc(sizeof(Item));
-  if(newItem==NULL){
-    return; }
-  newItem->str=strdup(str);
+  Item *newItem =(Item*)malloc(sizeof(Item));//allocate memory
+  if(newItem==NULL){//check if it failed to allocate memory
+    return;
+  }
+  newItem->str=strdup(str);//make copy
   if(newItem->str==NULL){
     free(newItem);
-    return; }
+    return;
+  }
 
-  newItem->next=NULL;
+  newItem->next=NULL;//set next to null
 
   if(list->root==NULL){
-    newItem->id=1;
+    newItem->id=1;//make the first item
     list->root = newItem;
     return;
   }
 
   Item *cur = list->root;
-  while(cur->next !=NULL){
+  while(cur->next !=NULL){//add everything else after the first item
     cur=cur->next;
   }
-  newItem->id= cur->id+1;
-  cur->next=newItem;
+  newItem->id= cur->id+1;//assign new id
+  cur->next=newItem;//set pointer of last item to new item
 
 }//end of add history
   
 
 char *get_history(List *list, int id){
   Item *cur=list->root;
-  if(cur==NULL){
-   return "No history stored";}
-  while(cur!=NULL){
-   if(cur->id==id){
-    return cur->str;}
-   cur=cur->next;}
+  if(cur==NULL){//check if empty
+   return "No history stored";
+  }
+  while(cur!=NULL){//go through the list
+    if(cur->id==id){
+      return cur->str;//return thr str field
+   }
+   cur=cur->next;
+  }
   return "Invalid Index";
 }//end of get history
  
 void print_history(List *list){
-  if(list->root ==NULL|| list==NULL){
+  if(list->root ==NULL|| list==NULL){//check if empty
     printf("List is empty\n");
   }
   printf("History: \n");
   
-  Item *cur = list->root;
+  Item *cur = list->root;//initialize to root of list
 
-   while(cur!=NULL){
+  while(cur!=NULL){//go through the list
      if(cur->str !=NULL){
-       printf("id: %d - input: %s\n",cur->id, cur->str);
+       printf("id %d = %s\n",cur->id, cur->str);
        cur = cur->next;
      }
      else{
-       printf("id: %d - input: NULL\n",cur->id);
+       printf("id %d = NULL\n",cur->id);
      }
    }
 }//end of print_history
 
 void free_history(List *list){
-  Item *trash = list->root;
-  while(trash !=NULL){
+  Item *trash = list->root;//initialize to root
+  while(trash !=NULL){//go through lust
     free(list->root);
     trash=trash->next;
-    list->root=list->root->next;
+    list->root=list->root->next;//update root pointer,remove current node
   }
   free(list);
 }//end of free_history
--- src/tokenizer.c
+++ src/tokenizer.c
@@ -15,38 +15,40 @@
   }//end of non_space_char
   
   char *token_start(char *tkn){
-    if(tkn==NULL||*tkn =='\0'){
-      return NULL;}
-    while(*tkn!='\0'&& space_char(*tkn)){
-      tkn++;}
-    return (*tkn=='\0') ?NULL :tkn;
+    if(tkn==NULL||*tkn =='\0'){//check if empty
+      return NULL;
+    }
+    while(*tkn!='\0'&& space_char(*tkn)){//continue pointer until terminator or not a white space
+      tkn++;
+    }
+    return (*tkn=='\0') ?NULL :tkn;//terminator return null else pointer to first character
   }//end of token_start
   
   int count_tokens(char *tkn){
     int count =0;
     char *start =tkn;
 
-    if(tkn==NULL){
+    if(tkn==NULL){//check if empty
       return 0;
     }
-    while((start =token_start(start)) != NULL){
+    while((start =token_start(start)) != NULL){//count tokens while theres tokens remain
       count ++;
-      start= token_terminator(start);
+      start= token_terminator(start);//identify next token
     }
     return count;
   }//end of count_tokens
   
   char *token_terminator(char *tkn){
-    while(*tkn != '\0' && non_space_char(*tkn)){
+    while(*tkn != '\0' && non_space_char(*tkn)){//go through characters
       tkn++;}
-    return tkn;
+    return tkn;//return then end of the token
   }//end of token_terminator
 
   char *copy_str(char *intStr, short len){
     char *copy = malloc((len+1)*sizeof(char));//allocate memory
 
     for (int i=0; i<len;i++){
-      copy[i]= intStr[i];/
+      copy[i]= intStr[i];
     }
     
     copy[len]='\0';
@@ -56,18 +58,18 @@
   char **tokenize(char *str){
     int total = count_tokens(str);
     char *token = str;
-    char **tokens = malloc(sizeof(char*) * (total+1));
+    char **tokens = malloc(sizeof(char*) * (total+1));//allocate memormy for token array
 
     for( int i=0; i<total;i++){
       
-      token=token_start(token);
-      char *tokEnd= token_terminator(token);
-      int len = tokEnd-token;
-      tokens[i]=copy_str(token,len);
-      token=tokEnd;
+      token=token_start(token);//pointer to start of token
+      char *tokEnd= token_terminator(token);//find end of current token
+      int len = tokEnd-token;//calculate the length
+      tokens[i]=copy_str(token,len);//copys into array index i
+      token=tokEnd;//pointer to start of next token
     }
     
-    tokens[total]='\0';
+    tokens[total]='\0';//mark the end of token list
     return tokens;
   }//end of tokenize
   
@@ -75,7 +77,7 @@
     if(tokens == NULL){
       return; }
     
-    for(int i=0; tokens[i] != NULL;i++){
+    for(int i=0; tokens[i] != NULL;i++){//traverse token array
       printf("Token[%d]: %s\n",i,tokens[i]);
     }
   }
@@ -83,7 +85,7 @@
   void free_tokens(char **tokens){
     if(tokens == NULL){
       return;}
-    for(int i=0;tokens[i] != NULL;i++){
+    for(int i=0;tokens[i] != NULL;i++){//traverse token array
       free(tokens[i]);
     }
     free(tokens);
